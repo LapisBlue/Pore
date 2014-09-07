7e0516dec8d15528f5f39303f95532f32e3f71b3
Maxim Roncace <mproncace@gmail.com>
2014-09-07 16:13:03 -0400

    Begin to convert API to bridge. This build is very, very broken.

--- src/main/java/net/amigocraft/pore/Main.java
+++ src/main/java/net/amigocraft/pore/Main.java
@@ -1,5 +1,26 @@
 package net.amigocraft.pore;
 
+import org.bukkit.Server;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 /**
  * @author Maxim Roncacé
  *
@@ -13,6 +34,70 @@ package net.amigocraft.pore;
  */
 public class Main {
 
-	//TODO: initialize as plugin
+	private static Server server;
+	private static PluginLoader loader;
+
+	private static List<JavaPlugin> plugins = new ArrayList<JavaPlugin>();
+
+	public static void initialize(){
+		//TODO: initialize as plugin
+
+		server = new Server();
+		loader = new JavaPluginLoader(server);
+
+		File serverDir = null; //TODO: use actual server directory
+		File bukkitDir = new File(serverDir, "bukkit-plugins");
+		for (File f : bukkitDir.listFiles()){
+			if (!f.isDirectory() && f.getName().endsWith(".jar")){
+				try {
+					JarFile pluginJar = new JarFile(f); // get JAR
+					ZipEntry pluginDesc = pluginJar.getEntry("plugin.yml"); // get plugin description
+					if (pluginDesc == null){ // not a plugin
+						System.err.println("[Pore] Failed to load plugin.yml for " + f.getName() + "!");
+						continue;
+					}
+					PluginDescriptionFile pdf = new PluginDescriptionFile(getInputStream(f, "plugin.yml"));
+					String main = pdf.getMain();
+					ClassLoader cLoader = URLClassLoader.newInstance(
+							new URL[]{f.toURI().toURL()},
+							Main.class.getClassLoader()
+					); // get Pore's classloader
+					Class<?> clazz = Class.forName(main, true, cLoader); // get the main class
+					Class<? extends JavaPlugin> pluginClass = clazz.asSubclass(JavaPlugin.class);
+					JavaPlugin plugin = pluginClass.cast(new JavaPlugin(loader, server, pdf, new File(bukkitDir, pdf.getName()), f));
+					plugin.onEnable(); // synthesize the standard onEnable call
+					plugins.add(plugin);
+				}
+				catch (InvalidDescriptionException ex){
+					ex.printStackTrace();
+					System.err.println("[Pore] Failed to load plugin description for " + f.getName() + "!");
+				}
+				catch (MalformedURLException ex){
+					ex.printStackTrace();
+					System.err.println("[Pore] An exception occurred while loading " + f.getName());
+				}
+				catch (ClassNotFoundException ex){
+					ex.printStackTrace();
+					System.err.println("[Pore] Failed to load main class for " + f.getName() + "!");
+				}
+				catch (IOException ex){
+					ex.printStackTrace();
+					System.err.println("[Pore] Failed to load " + f.getName() + "!");
+				}
+			}
+		}
+	}
+
+	private static InputStream getInputStream(File zip, String entry) throws IOException {
+		ZipInputStream zin = new ZipInputStream(new FileInputStream(zip));
+		for (ZipEntry e; (e = zin.getNextEntry()) != null;) {
+			if (e.getName().equals(entry)) {
+				return zin;
+			}
+		}
+		throw new EOFException("Cannot find " + entry);
+	}
+
+	//TODO: disable plugins
 
 }
