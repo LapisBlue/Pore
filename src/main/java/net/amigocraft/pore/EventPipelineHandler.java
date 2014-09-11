57f9cc221939ee57d9f33d44860c640308dc742a
Maxim Roncace <mproncace@gmail.com>
2014-09-11 15:55:41 -0400

    Add listener for AsyncPlayerChatEvent Implement all possible methods for Player and all superclasses

--- src/main/java/net/amigocraft/pore/EventPipelineHandler.java
+++ src/main/java/net/amigocraft/pore/EventPipelineHandler.java
@@ -2,31 +2,46 @@ package net.amigocraft.pore;
 
 import net.amigocraft.pore.implementation.PoreChunk;
 import net.amigocraft.pore.implementation.PoreWorld;
+import net.amigocraft.pore.implementation.entity.PorePlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.*;
 import org.spongepowered.api.event.SpongeEventHandler;
+import org.spongepowered.api.event.player.*;
+import org.spongepowered.api.event.player.AsyncPlayerChatEvent;
 import org.spongepowered.api.event.world.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class EventPipelineHandler {
 
 	@SpongeEventHandler
-	public void onChunkLoad(ChunkLoadEvent event) { //TODO: instanceof WorldEvent according to Bukkit, but not Sponge. wat do?
+	public void onChunkLoad(ChunkLoadEvent event) { //TODO: instanceof WorldEvent according to Bukkit, but not Sponge
 		//TODO: fix second argument when Sponge makes it possible
-		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.ChunkLoadEvent(new PoreChunk(event.chunk), false));
+		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.ChunkLoadEvent(new PoreChunk(event.getChunk()), false));
 	}
 
 	@SpongeEventHandler
 	public void onChunkUnload(ChunkUnloadEvent event) {
-		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.ChunkUnloadEvent(new PoreChunk(event.chunk)));
+		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.ChunkUnloadEvent(new PoreChunk(event.getChunk())));
 	}
 
 	@SpongeEventHandler
 	public void onWorldEvent(WorldLoadEvent event) {
-		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.WorldLoadEvent(new PoreWorld(event.world)));
+		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.WorldLoadEvent(new PoreWorld(event.getWorld())));
 	}
 
 	@SpongeEventHandler
 	public void onWorldUnload(WorldUnloadEvent event) {
-		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.WorldUnloadEvent(new PoreWorld(event.world)));
+		Bukkit.getPluginManager().callEvent(new org.bukkit.event.world.WorldUnloadEvent(new PoreWorld(event.getWorld())));
 	}
+
+    @SpongeEventHandler
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event){
+        Bukkit.getPluginManager().callEvent(
+				new org.bukkit.event.player.AsyncPlayerChatEvent(
+						false, new PorePlayer(event.getPlayer()), event.getMessage(), new HashSet<Player>())); //TODO
+    }
 
 }
--- src/main/java/net/amigocraft/pore/implementation/entity/PoreLivingEntity.java
+++ src/main/java/net/amigocraft/pore/implementation/entity/PoreLivingEntity.java
@@ -6,6 +6,9 @@ import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.inventory.EntityEquipment;
@@ -14,217 +17,25 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
+import org.spongepowered.api.entity.*;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 
+//TODO: bridge
 public class PoreLivingEntity extends PoreEntity implements LivingEntity {
 
-	// TODO: Bridge
+	private org.spongepowered.api.entity.LivingEntity handle;
 
-	// Overrided from Entity
-
-	@Override
-	public Location getLocation() {
-		return super.getLocation();
-	}
-
-	@Override
-	public Location getLocation(Location loc) {
-		return super.getLocation(loc);
-	}
-
-	@Override
-	public void setVelocity(Vector velocity) {
-		super.setVelocity(velocity);
-	}
-
-	@Override
-	public Vector getVelocity() {
-		return super.getVelocity();
-	}
-
-	@Override
-	public boolean isOnGround() {
-		return super.isOnGround();
-	}
-
-	@Override
-	public World getWorld() {
-		return super.getWorld();
-	}
-
-	@Override
-	public boolean teleport(Location location) {
-		return super.teleport(location);
-	}
-
-	@Override
-	public boolean teleport(Location location, PlayerTeleportEvent.TeleportCause cause) {
-		return super.teleport(location, cause);
-	}
-
-	@Override
-	public boolean teleport(Entity destination) {
-		return super.teleport(destination);
-	}
-
-	@Override
-	public boolean teleport(Entity destination, PlayerTeleportEvent.TeleportCause cause) {
-		return super.teleport(destination, cause);
-	}
-
-	@Override
-	public List<Entity> getNearbyEntities(double x, double y, double z) {
-		return super.getNearbyEntities(x, y, z);
-	}
-
-	@Override
-	public int getEntityId() {
-		return super.getEntityId();
-	}
-
-	@Override
-	public int getFireTicks() {
-		return super.getFireTicks();
-	}
-
-	@Override
-	public int getMaxFireTicks() {
-		return super.getMaxFireTicks();
-	}
-
-	@Override
-	public void setFireTicks(int ticks) {
-		super.setFireTicks(ticks);
-	}
-
-	@Override
-	public void remove() {
-		super.remove();
-	}
-
-	@Override
-	public boolean isDead() {
-		return super.isDead();
-	}
-
-	@Override
-	public boolean isValid() {
-		return super.isValid();
-	}
-
-	@Override
-	public Server getServer() {
-		return super.getServer();
-	}
-
-	@Override
-	public Entity getPassenger() {
-		return super.getPassenger();
-	}
-
-	@Override
-	public boolean setPassenger(Entity passenger) {
-		return super.setPassenger(passenger);
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return super.isEmpty();
-	}
-
-	@Override
-	public boolean eject() {
-		return super.eject();
-	}
-
-	@Override
-	public float getFallDistance() {
-		return super.getFallDistance();
-	}
-
-	@Override
-	public void setFallDistance(float distance) {
-		super.setFallDistance(distance);
-	}
-
-	@Override
-	public void setLastDamageCause(EntityDamageEvent event) {
-		super.setLastDamageCause(event);
-	}
-
-	@Override
-	public EntityDamageEvent getLastDamageCause() {
-		return super.getLastDamageCause();
-	}
-
-	@Override
-	public UUID getUniqueId() {
-		return super.getUniqueId();
-	}
-
-	@Override
-	public int getTicksLived() {
-		return super.getTicksLived();
-	}
-
-	@Override
-	public void setTicksLived(int value) {
-		super.setTicksLived(value);
-	}
-
-	@Override
-	public void playEffect(EntityEffect type) {
-		super.playEffect(type);
-	}
-
-	@Override
-	public EntityType getType() {
-		return super.getType();
-	}
-
-	@Override
-	public boolean isInsideVehicle() {
-		return super.isInsideVehicle();
-	}
-
-	@Override
-	public boolean leaveVehicle() {
-		return super.leaveVehicle();
-	}
-
-	@Override
-	public Entity getVehicle() {
-		return super.getVehicle();
-	}
-
-	@Override
-	public void setMetadata(String metadataKey, MetadataValue newMetadataValue) {
-		super.setMetadata(metadataKey, newMetadataValue);
-	}
-
-	@Override
-	public List<MetadataValue> getMetadata(String metadataKey) {
-		return super.getMetadata(metadataKey);
-	}
-
-	@Override
-	public boolean hasMetadata(String metadataKey) {
-		return super.hasMetadata(metadataKey);
-	}
-
-	@Override
-	public void removeMetadata(String metadataKey, Plugin owningPlugin) {
-		super.removeMetadata(metadataKey, owningPlugin);
+	public PoreLivingEntity(org.spongepowered.api.entity.LivingEntity handle) {
+		super(handle);
 	}
 
 	// Overrided from LivingEntity
 
-
-	@Override
+ 	@Override
 	public double getEyeHeight() {
 		return 0;
 	}
@@ -433,12 +244,12 @@ public class PoreLivingEntity extends PoreEntity implements LivingEntity {
 
 	@Override
 	public void damage(double amount) {
-
+		((org.spongepowered.api.entity.LivingEntity)getHandle()).damage(amount);
 	}
 
 	@Override
 	public void _INVALID_damage(int amount) {
-
+		((org.spongepowered.api.entity.LivingEntity)getHandle()).damage((double)amount);
 	}
 
 	@Override
@@ -453,22 +264,22 @@ public class PoreLivingEntity extends PoreEntity implements LivingEntity {
 
 	@Override
 	public double getHealth() {
-		return 0;
+		return ((org.spongepowered.api.entity.LivingEntity)getHandle()).getHealth();
 	}
 
 	@Override
 	public int _INVALID_getHealth() {
-		return 0;
+		return (int)((org.spongepowered.api.entity.LivingEntity)getHandle()).getHealth();
 	}
 
 	@Override
 	public void setHealth(double health) {
-
+		((org.spongepowered.api.entity.LivingEntity)getHandle()).setHealth(health);
 	}
 
 	@Override
 	public void _INVALID_setHealth(int health) {
-
+		((org.spongepowered.api.entity.LivingEntity)getHandle()).setHealth(health);
 	}
 
 	@Override
