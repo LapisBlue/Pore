2d4b68b1b2ac6f3de012541af476f25ecf38c35d
gratimax <max@ovsankin.com>
2014-09-27 19:41:59 -0700

    try for travis

---
+++ extra/scripts/travis_javadocs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$TRAVIS_JDK_VERSION" = "oraclejdk8" ]; then
+    git clone https://${GH_TOKEN}@github.com/LapisBlue/Javadocs .jd
+    mvn javadoc:javadoc && {
+        cd .jd/
+        git config user.name "Lapislazuli"
+        git config user.email "lapislazuli@lapis.blue"
+        git rm -r pore
+        cp -r ../target/site/apidocs/ pore
+        git add -A
+        git commit -m "Update to $(date)"
+        git push origin gh-pages
+    }
+fi
