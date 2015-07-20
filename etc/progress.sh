cdccc2cca04ec7401c369335c0236807516fa2e1
caseif <caseif@caseif.net>
2015-07-19 22:10:23 -0400

    Actually fix progress.sh on MSYS

--- etc/progress.sh
+++ etc/progress.sh
@@ -5,8 +5,8 @@ OVERRIDE_STR="@Override"
 NOT_IMPLD_STR="throw new NotImplementedException"
 
 # grep matches
-OVERRIDES=`grep -r "$OVERRIDE_STR" $IMPL_DIR | wc -l | cut -f1 -d ' '`
-NOT_IMPLD=`grep -r "$NOT_IMPLD_STR" $IMPL_DIR | wc -l | cut -f1 -d ' '`
+OVERRIDES=`grep -r "$OVERRIDE_STR" $IMPL_DIR | wc -l | tr -d ' '`
+NOT_IMPLD=`grep -r "$NOT_IMPLD_STR" $IMPL_DIR | wc -l | tr -d ' '`
 
 # math to get a percentage (yay integer math :P)
 IMPLD_FRACTION=$[($OVERRIDES - $NOT_IMPLD) * 10000 / $OVERRIDES]
