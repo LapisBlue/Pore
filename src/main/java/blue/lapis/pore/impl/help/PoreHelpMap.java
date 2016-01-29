a96865ef4b441e87219369db330a77b001f9f965
Minecrell <dev@minecrell.net>
2016-01-29 18:47:20 +0100

    Add dummy HelpMap implementation. Fixes #27

--- src/main/java/blue/lapis/pore/impl/help/PoreHelpMap.java
+++ src/main/java/blue/lapis/pore/impl/help/PoreHelpMap.java
@@ -24,7 +24,8 @@
  */
 package blue.lapis.pore.impl.help;
 
-import org.apache.commons.lang3.NotImplementedException;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import org.bukkit.help.HelpMap;
 import org.bukkit.help.HelpTopic;
 import org.bukkit.help.HelpTopicFactory;
@@ -36,32 +37,29 @@ public class PoreHelpMap implements HelpMap {
 
     @Override
     public HelpTopic getHelpTopic(String topicName) {
-        throw new NotImplementedException("TODO");
+        return null;
     }
 
     @Override
     public Collection<HelpTopic> getHelpTopics() {
-        throw new NotImplementedException("TODO");
+        return ImmutableSet.of();
     }
 
     @Override
     public void addTopic(HelpTopic topic) {
-        throw new NotImplementedException("TODO");
     }
 
     @Override
     public void clear() {
-        throw new NotImplementedException("TODO");
     }
 
     @Override
     public void registerHelpTopicFactory(Class<?> commandClass, HelpTopicFactory<?> factory) {
-        throw new NotImplementedException("TODO");
     }
 
     @Override
     public List<String> getIgnoredPlugins() {
-        throw new NotImplementedException("TODO");
+        return ImmutableList.of();
     }
 
 }
